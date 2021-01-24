b51ae1d74badd858817907fc58ff110f89b74344
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-24 19:14:04 -0300

    chore: java scope

---
+++ sintaxe-variavel-fluxo/TestaEscopo.java
@@ -0,0 +1,26 @@
+public class TestaEscopo { 
+
+    public static void main(String[] args) { 
+    
+    System.out.println("Testando condicionais"); 
+    
+     
+    
+    int idade = 20; 
+    
+    int quantidadePessoas = 3; 
+    
+     
+    boolean acompanhado = quantidadePessoas >= 2; //does not accept 0/1 
+    
+     
+    if (idade >= 18 && acompanhado) { 
+    
+        System.out.println("Seja bem-vindo!"); 
+        
+        } else { 
+        
+        System.out.println("Você não pode entrar."); 
+        } 
+    } 
+} 
