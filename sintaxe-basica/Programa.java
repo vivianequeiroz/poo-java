94bc717ce23a4649eefa4c9ee0e1c8e503b91a8a
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-01-24 19:06:00 -0300

    chore: general syntax

--- sintaxe-basica/Programa.java
+++ sintaxe-basica/Programa.java
@@ -3,5 +3,7 @@ public class Programa {
     public static void main(String[] args) { 
 
         System.out.println("Ola mundo"); 
+        System.out.println("Primeiro hello world com Java");
+
     } 
 } 
