8bc5a0bd1cf1ae58944ff7f2205b62bf4dc9d07e
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2021-01-26 10:31:27 -0300

    chore: private attributes

--- bytebank-composto/src/Conta.java
+++ bytebank-composto/src/Conta.java
@@ -1,5 +1,5 @@
 public class Conta {
-	double saldo;
+	private double saldo;
 	int agencia;
 	int numero;
 	Cliente titular;
@@ -27,6 +27,10 @@ public class Conta {
 		} else {
 			return false;
 		}
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
 	}
 
 	//methods
--- bytebank-composto/src/TestaContaSemCliente.java
+++ bytebank-composto/src/TestaContaSemCliente.java
@@ -3,7 +3,9 @@ public class TestaContaSemCliente {
 
 	public static void main(String[] args) {
 		Conta contaDoJoao = new Conta();
-		System.out.println(contaDoJoao.saldo);
+		System.out.println(contaDoJoao.getSaldo());
+		// a new method (pegaSaldo) invoked invoked
+		// to access the saldo due to the addition of "private" in the class Conta attributes
 		
 		contaDoJoao.titular = new Cliente();
 		System.out.println(contaDoJoao.titular);
