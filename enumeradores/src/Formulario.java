ae5e57639410ba50fa76d43d1afce7eb0a68f7e1
Viviane Queiroz <vivianedsqueiroz@gmail.com>
2021-01-26 17:33:49 -0300

    chore: enumerators and bigdecimal and biginteger

---
+++ enumeradores/src/Formulario.java
@@ -0,0 +1,18 @@
+package enumeradores;
+
+public class Formulario {
+	
+	enum Genero {
+		FEMININO('F'), MASCULINO('M');
+		
+		private char valor; 
+		
+		Genero(char valor) {
+			this.valor = valor;
+		}
+	}
+	
+	private String nome;
+	private Genero genero;
+
+}
