8087c864b64a5c851ffea9c1fa239c38cc8f4f6f
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-30 15:22:23 -0300

    chore: throw for inserintg exceptions at the call stack

---
+++ exceções/fluxo-java/throw.java
@@ -0,0 +1,29 @@
+public class FluxoComTratamento {
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        try {
+        	  metodo2();
+        } catch(ArithmeticException | NullPointerException ex) {
+        	String msg = ex.getMessage();
+        	System.out.println("Exception " + msg);
+        	ex.printStackTrace();
+        } 
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        
+        // ArithmeticException exception = new ArithmeticException("houve um erro :(");
+        throw new ArithmeticException("houve um erro :(");
+       
+        //System.out.println("Fim do metodo2");
+    }
+}
